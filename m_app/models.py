be0ae374bd1893f50200bad07801507483f375db
Mussa Nash Nzumbi <g4ce60@gmail.com>
2023-06-19 18:16:00 +0300

    Models Update

--- m_app/models.py
+++ m_app/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+class StationName(models.Model):
+    pass
+
+class EmployeeData(AbstracttUser):
+    pass
+
+class ClientData(models.Model):
+    pass
+
+class MaternityData(models.Model):
+    pass
+
+class MenstruationData(models.Model):
+    pass
+
+class SpecialTestData(models.Model):
+    pass
+
+class ReportsData(models.model):
+    pass
+
+
+class Notification(models.Model):
+    pass
+
+
