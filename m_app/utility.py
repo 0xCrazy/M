50f9a05631b32ccfd31aaa0abc4a6b5180ee09f4
Mussa Nash Nzumbi <g4ce60@gmail.com>
2023-06-19 18:22:57 +0300

    -

---
+++ m_app/utility.py
@@ -0,0 +1,24 @@
+"""
+
+Authorization based on groups
+1. Clinical
+        Doctor
+        Nurse
+        Matron/Nurse Manager
+        Social Worker
+2. Administration
+        Hospital Administrator
+        Business Office/Accounts
+        Inventory manager
+        Patient Administrator
+3. Support
+        Imaging Technician
+        Lab Technician
+        Pharmacist
+        ICT Officer
+4. Normal User 
+        Clients - (Pregnant Mother)
+
+
+
+"""
