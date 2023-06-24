fc1499b4824aabe0dd50b21aa95151923c74dbb6
Mussa Nash Nzumbi <g4ce60@gmail.com>
2023-06-24 17:41:23 +0300

    -

--- m_app/models.py
+++ m_app/models.py
@@ -1,5 +1,6 @@
+from typing import Any, Optional
 from django.db import models
-from django.contrib.auth.models import UserManager
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager
 
 # Create your models here.
 
@@ -7,7 +8,41 @@ class StationName(models.Model):
     pass
 
 class EmployeeDataManager(UserManager):
-    pass
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('Weka Barua Pepe Sahihi')
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+    
+    def create_user(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+    
+
+    def create_superuser(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self._create_user(email, password, **extra_fields)
+    
+
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(blank=True, default='', unique=True)
+    name = models.CharField(max_length=250, blank=True, default='')
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+
+
 
 class ClientData(models.Model):
     pass
